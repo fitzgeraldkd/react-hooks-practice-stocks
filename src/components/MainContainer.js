1374b0377e8ae0518ffc611d0b4db9e36d9eb30b
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-10-20 21:19:53 -0400

    commit tree e0cd6874f31c618447fd4e03294335f3cc83189c

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sort, setSort] = useState();
+  const [filter, setFilter] = useState()
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+      .then(resp => resp.json())
+      .then(data => setStocks(data));
+  }, []);
+
+  const buyStock = (stock) => {
+    if (!portfolio.includes(stock)) {
+      setPortfolio(currentPortfolio => [...currentPortfolio, stock]);
+    }
+  };
+
+  const sellStock = (stock) => {
+    setPortfolio(currentPortfolio => currentPortfolio.filter(portfolioStock => 
+      portfolioStock.id !== stock.id
+    ))
+  };
+
+  const handleSort = (e) => {
+    setSort(e.target.value);
+  };
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const displayStocks = stocks
+    .filter(stock => stock.type === filter || !filter)
+    .sort((a, b) => {
+    if (a[sort] > b[sort]) return 1;
+    if (a[sort] < b[sort]) return -1;
+    return 0
+  });
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar sort={sort} filter={filter} handleSort={handleSort} handleFilter={handleFilter} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={displayStocks} buyStock={buyStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} sellStock={sellStock} />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,33 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ sort, filter, handleSort, handleFilter }) {
   return (
     <div>
       <strong>Sort by:</strong>
       <label>
         <input
           type="radio"
-          value="Alphabetically"
+          value="name"
           name="sort"
           checked={null}
-          onChange={null}
+          onChange={handleSort}
         />
         Alphabetically
       </label>
       <label>
         <input
           type="radio"
-          value="Price"
+          value="price"
           name="sort"
           checked={null}
-          onChange={null}
+          onChange={handleSort}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleFilter} value={filter}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
